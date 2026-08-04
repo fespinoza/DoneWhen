5f699b8fc6d9e06f318c1592a45adb00297e2e9c
Felipe Espinoza <fespinozacast@gmail.com>
2026-08-04 20:54:29 +0200

    Initial Commit

---
+++ DoneWhen/DoneWhenApp.swift
@@ -0,0 +1,17 @@
+//
+//  DoneWhenApp.swift
+//  DoneWhen
+//
+//  Created by Felipe Espinoza on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct DoneWhenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
